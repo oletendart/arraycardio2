de328363a2bf330beec21fe2943e549847b679c8
Olivia Letendart <letendartolivia@gmail.com>
2020-01-21 11:27:30 +0100

    create const allAdults return true or false

--- script.js
+++ script.js
@@ -28,4 +28,16 @@ const isAdult = people.some(
   person => new Date().getFullYear() - person.year >= 19
 );
 
-console.log(isAdult);
+console.log({ isAdult });
+
+// Array.prototype.every() // is everyone 19 ?
+
+const allAdults = people.every(
+  person => new Date().getFullYear() - person.year >= 19
+);
+
+console.log({ allAdults });
+
+// Array.prototype.find()
+// Find is like filter, but instead returns just the one you are looking for
+// find the comment with the ID of 823423
