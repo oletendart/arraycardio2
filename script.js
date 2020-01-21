5ed3208cf39c9b80087329a195deb10dcaee0149
Olivia Letendart <letendartolivia@gmail.com>
2020-01-21 11:32:40 +0100

    refactorisation + simplification const comment

--- script.js
+++ script.js
@@ -41,9 +41,6 @@ console.log({ allAdults });
 // Array.prototype.find()
 // Find is like filter, but instead returns just the one you are looking for
 // find the comment with the ID of 823423
-const comment = comments.find(function(comment) {
-  if (comment.id === 823423) {
-    return true;
-  }
-});
+const comment = comments.find(comment => comment.id === 823423);
+
 console.log(comment);
