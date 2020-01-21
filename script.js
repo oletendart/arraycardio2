1eba459b2eb8fab6f83bf40eb63afbb77f250290
Olivia Letendart <letendartolivia@gmail.com>
2020-01-21 11:40:59 +0100

    create const slice the comments

--- script.js
+++ script.js
@@ -53,4 +53,6 @@ const index = comments.findIndex(comment => comment.id === 823423);
 
 console.log(index);
 
-comments.splice(index, 1);
+// comments.splice(index, 1);
+
+const newComments = [...comments.slice(0, index), ...comments.slice(index + 1)];
